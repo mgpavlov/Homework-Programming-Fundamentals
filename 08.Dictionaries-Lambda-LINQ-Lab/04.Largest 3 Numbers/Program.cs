039b1e97771bede07e726af9b810984f99909088
Mihail Pavlov <lookingup@abv.bg>
2017-10-10 17:58:34 +0300

    add tasks 4 and 5.

---
+++ 08.Dictionaries-Lambda-LINQ-Lab/04.Largest 3 Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            var output = list.OrderByDescending(x => x).Take(3);
+            Console.WriteLine(String.Join(" ", output));
+        }
+    }
+}
---
+++ 08.Dictionaries-Lambda-LINQ-Lab/05.Short Words Sorted/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Short_Words_Sorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var separator = new char[] { '.', ',', ':', ';', '(', ')', '[', ']', '\"', '\'', '\\', '/', '!',' ', '?'};
+            var list = Console.ReadLine().ToLower().Split(separator).ToList();
+            var orderedByWordLength = list.Where(w => w !="").Where(w => w.Length < 5).OrderBy(w => w).Distinct();
+            Console.WriteLine(String.Join(", ", orderedByWordLength));
+        }
+    }
+}
