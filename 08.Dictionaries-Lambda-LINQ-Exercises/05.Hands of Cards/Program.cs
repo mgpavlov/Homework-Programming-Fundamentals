40a15c490b69c4373f3fa9b54d0f7bdfb1d95cd6
mgpavlov <m.g.pavlov@abv.bg>
2017-10-12 23:58:03 +0300

    add task 8.

--- 08.Dictionaries-Lambda-LINQ-Exercises/05.Hands of Cards/Program.cs
+++ 08.Dictionaries-Lambda-LINQ-Exercises/05.Hands of Cards/Program.cs
@@ -10,6 +10,53 @@ namespace _05.Hands_of_Cards
     {
         static void Main(string[] args)
         {
+            var line = Console.ReadLine();
+            var namesCards = new Dictionary<string, List <int>>();
+            while (line != "JOKER")
+            {
+                var tokens = line.Split(':');
+                var name = tokens [0];
+                var cards = tokens[1]
+                    .Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(a => CalculateCardValue(a))
+                    .ToList();
+                if (!namesCards.ContainsKey(name))
+                {
+                    namesCards[name] = new List<int>();
+                }
+                namesCards[name].AddRange(cards);
+
+                line = Console.ReadLine();
+            }
+            foreach (var nameCards in namesCards)
+            {
+                var name = nameCards.Key;
+                var cards = nameCards.Value;
+                var totalCardsSum = cards.Distinct().Sum();
+                Console.WriteLine($"{name}: {totalCardsSum}");
+            }
+        }
+
+        static int CalculateCardValue(string card)
+        {
+            var cardPowers = new Dictionary<string, int>();
+                cardPowers["J"] = 11;
+                cardPowers["Q"] = 12;
+                cardPowers["K"] = 13;
+                cardPowers["A"] = 14;
+                for (int i = 2; i < 11; i++)
+                {
+                    cardPowers[i.ToString()] = i;
+                }
+            var cardTypes = new Dictionary<string, int>();
+            cardTypes["S"] = 4;
+            cardTypes["H"] = 3;
+            cardTypes["D"] = 2;
+            cardTypes["C"] = 1;
+
+            var power = card.Substring(0, card.Length-1);
+            var type = card.Substring(card.Length-1);
+            var value = cardPowers[power] * cardTypes[type];
+            return value;
         }
     }
 }
